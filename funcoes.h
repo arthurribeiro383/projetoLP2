465fdbb3aa0b5ce26fe7abb001c8d2931f1b1564
arthurribeiro383 <arthurribeiro383@gmail.com>
2025-12-08 22:07:14 -0300

    Organização melhor no Func.H

--- funcoes.h
+++ funcoes.h
@@ -5,14 +5,13 @@
 #define MAX_NOTI 3 //maximo de notificações diferentes
 typedef void(*notificar)(const char*); //tipo 'notificar' é uma função void que recebe char*.
 
+//ESTRUTURAS:
 typedef struct custom_noti{
     int option;
     char descricao[20];
     notificar alerta;
 }custom_noti;
 
-
-#define ESGOTADO 1
 typedef struct
 {
     int id;
@@ -22,17 +21,6 @@ typedef struct
     int quantidade;
 } Tproduto;
 
-extern int qtdProdutos; //Contador de produtos cadastrados (variavel global)
-
-
-//_______________________________________________________________________________________________________________
-//Protótipos das funções:
-Tproduto* cadastrarProduto(Tproduto produto[]);
-void listarProdutos(Tproduto produtos[]);
-Tproduto atualizarEstoque(Tproduto produtos[]);
-void deletarProduto(Tproduto[], int id);
-int buscaProduto_ID(Tproduto[], int id); //Busca o produto por ID e retorna indice. "-1 = Produto nao encontrado"
-
 typedef struct Toferta{
     char vendedor[50];
     char nome[50];
@@ -41,22 +29,29 @@ typedef struct Toferta{
     struct Toferta* prox;
 } Toferta;
 
-//Função para executar e controlar o menu de vendas
-//Parâmetro de entrada: ptr para cabeça da lista(preferencial criar no main)
-void vendasMenu(Toferta*);
-void registrar_oferta(Toferta*);  //parametros:  ptr para cabeca
+//FUNCOES DO ESTOQUE: 
+Tproduto* cadastrarProduto(Tproduto produto[]);
+void listarProdutos(Tproduto produtos[]);
+Tproduto atualizarEstoque(Tproduto produtos[]);
+void deletarProduto(Tproduto[], int id);
+int buscaProduto_ID(Tproduto[], int id); //Busca o produto por ID e retorna indice. "-1 = Produto nao encontrado"
+
+
+//FUNCOES DE OFERTA:
+void vendasMenu(Toferta*); //Função para executar e controlar o menu de vendas
+void registrar_oferta(Toferta*);  //Parâmetro de entrada: ptr para cabeça da lista(preferencial criar no main)
 void excluir_oferta(Toferta*);
 void lista_ofertas(Toferta*);  //imprime todas as ofertas
 void liberaLista(Toferta*);  //libera a lista inteira EXCETO A CABEÇA(liberar no main ou modificar função)Isso facilita reutilização da cabeça se necessário
 
+//FUNCOES DE ALERTA.
 void alerta_email(const char* msg);
 void alerta_sistema(const char* msg);
 void alerta_cel(const char* msg);
-
 void registrar_alerta_custom(notificar, char*); //registra um alerta + uma descricao
 void option_alerta_custom(void); //verifica se o usuario quer ou nao aquele alerta. Deve ser usada sempre imediatamente antes de notificar_all_custom
 void notificar_all_custom(const char*); //Chama ou nao alguma funcao de alerta
 
-//_______________________________________________________________________________________________________________
+extern int qtdProdutos; //Contador de produtos cadastrados (variavel global)
 
 #endif
